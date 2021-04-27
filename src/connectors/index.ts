dd80b565a8b637ba22b378b938ed24a070f79e82
financelap <82516958+financelap@users.noreply.github.com>
2021-04-27 16:07:47 +0700

    Update index.ts

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -11,7 +11,7 @@ const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
 
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '56')
 
 if (typeof NETWORK_URL === 'undefined') {
   throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
@@ -27,27 +27,27 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+  supportedChainIds: [1, 3, 4, 5, 42, 56]
 })
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: NETWORK_URL },
-  bridge: 'https://bridge.walletconnect.org',
+  rpc: { 56: NETWORK_URL },
+  bridge: 'https://bsc-dataseed.binance.org/',
   qrcode: true,
   pollingInterval: 15000
 })
 
 // mainnet only
 export const fortmatic = new FortmaticConnector({
-  apiKey: FORMATIC_KEY ?? '',
-  chainId: 1
+  apiKey: FORMATIC_KEY ?? 'https://bsc-dataseed1.defibit.io/',
+  chainId: 56
 })
 
 // mainnet only
 export const portis = new PortisConnector({
-  dAppId: PORTIS_ID ?? '',
-  networks: [1]
+  dAppId: PORTIS_ID ?? 'https://bsc-dataseed1.ninicoin.io/',
+  networks: [56]
 })
 
 // mainnet only
